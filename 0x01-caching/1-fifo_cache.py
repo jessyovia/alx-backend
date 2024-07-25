44310d97a381a70f36558d0896998e7a32a9ceb5
jessyovia <jessyovia100@gmail.com>
2024-07-25 00:10:46 +0000

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
-""" FIFOCache module
-"""
+
+
+"""FIFOCache Module"""
+
+
 from base_caching import BaseCaching
 from collections import OrderedDict
 
-class FIFOCache(BaseCaching):
-    """ FIFOCache class inherits from BaseCaching
-    """
 
+class FIFOCache(BaseCaching):
+    """FIFOCache class - FIFO caching system"""
     def __init__(self):
-        """ Initialize
-        """
+        """Initialize FIFOCache"""
         super().__init__()
-        self.order = OrderedDict()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item to the cache
-        """
-        if key and item:
-        if key in self.cache_data:
-        self.order.move_to_end(key)
-        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded = self.order.popitem(last=False)
-                print(f"DISCARD: {discarded[0]}")
-            self.cache_data.pop(discarded[0])
-            self.cache_data[key] = item
-            self.order[key] = None
+        """Adds item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            fst, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {fst}")
 
     def get(self, key):
-        """ Get an item from the cache
-        """
-        if key is None or key not in self.cache_data:
-            return None
-        self.order.move_to_end(key)
-        return self.cache_data[key]
+        """Gets item by key"""
+        return self.cache_data.get(key, None)
